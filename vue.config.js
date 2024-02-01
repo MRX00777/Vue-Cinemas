76a2ecbda63f23cbb63c552c32bc2cef9a20d040
MRX007 <mrx31995@gmail.com>
2024-02-01 11:36:07 +0500

    добавил gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,24 @@
-const { defineConfig } = require('@vue/cli-service')
+const {
+  defineConfig
+} = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: `/Vue-Cinemas/`,
   transpileDependencies: true
 })
 module.exports = {
   configureWebpack: {
     module: {
-      rules: [
-        {
-          test: /\.pug$/,
-          oneOf: [
-            {
-              resourceQuery: /^\?vue/,
-              use: ['pug-plain-loader']
-            },
-            {
-              use: ['raw-loader', 'pug-plain-loader']
-            }
-          ]
-        }
-      ]
+      rules: [{
+        test: /\.pug$/,
+        oneOf: [{
+            resourceQuery: /^\?vue/,
+            use: ['pug-plain-loader']
+          },
+          {
+            use: ['raw-loader', 'pug-plain-loader']
+          }
+        ]
+      }]
     }
   }
 }
